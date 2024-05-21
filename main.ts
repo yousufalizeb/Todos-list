bf16ace9641b8aa55036db087ac6d8458d1b19b0
Yousuf Ali Zeb <yousufalizeb23@gmail.com>
2024-05-22 00:44:18 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer"
+let todos =[];
+let condition = true
+while(condition){
+    let addTask =await inquirer.prompt(
+         [
+            {
+                name:"todo",
+                type:"input",
+                message:"eid ki shopping kya kya ki  "
+            },
+            {
+                name:"addmore",
+                type:"confirm",
+                message:"aur kea kharida",
+                default:"false"
+            }
+         ]
+    ) 
+    todos.push (addTask.todo);
+    condition=addTask.addmore
+    console.log(todos)
+}
